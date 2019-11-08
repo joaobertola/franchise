60b73b7e1aecc1a5d1da597752d3c77eac2e9a48
Note-Luciano\Luciano <lucianomancini@hotmail.com>
2019-11-08 17:56:29 -0200

    Acerto SQL

--- php/graficos/App/Grafico_Franquia_25.php
+++ php/graficos/App/Grafico_Franquia_25.php
@@ -6,58 +6,57 @@ include("Includes/PageLayout.php");
 include("DataGen.php");
 ?>
 <HTML>
+
 <HEAD>
-<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<TITLE>
-		Gráficos WEB CONTROL EMPRESAS
-	</TITLE>
+    <TITLE>
+        Gráficos WEB CONTROL EMPRESAS
+    </TITLE>
     <style>
 
     </style>
 
     <SCRIPT LANGUAGE="Javascript" SRC="js/jquery.min.js"></SCRIPT>
-    <SCRIPT LANGUAGE="Javascript" SRC="js/loader.js"></SCRIPT>	
+    <SCRIPT LANGUAGE="Javascript" SRC="js/loader.js"></SCRIPT>
 </HEAD>
+
 <BODY topmargin='0' leftmargin='0' bottomMargin='0' rightMargin='0' bgColor='#EEEEEE'>
 
-<?php
-	//Render page headers
-	if (!empty($_REQUEST['franqueado'])){
-		echo render_pageHeader();
-	}
-?>
+    <?php
+    //Render page headers
+    if (!empty($_REQUEST['franqueado'])) {
+        echo render_pageHeader();
+    }
+    ?>
 
-<table align='center' cellspacing='0' cellpadding='0'>
-    	<td align='left'>
-        <?php if (!empty($_REQUEST['franqueado'])){ ?>
-        <a href="../../painel.php?pagina1=graficos/i_graficos.php">Retorna</a>
-        <?php } ?>
-        </td>
-    </tr>
-	<tr>
-	<td align='center'>
+    <table align='center' cellspacing='0' cellpadding='0'>
+        <tr>
+            <td align='left'>
+                <?php if (!empty($_REQUEST['franqueado'])) { ?>
+                <a href="../../painel.php?pagina1=graficos/i_graficos.php">Retorna</a>
+                <?php } ?>
+            </td>
+        </tr>
+        <tr>
+            <td align='center'>
+                <?php
 
-	<?php
-	
-	$FC = new FusionCharts("Column3D",$comprimento,$largura," ");
-	$FC -> setSWFPath("FusionCharts/");
+                $FC = new FusionCharts("Column3D", $comprimento, $largura, " ");
+                $FC->setSWFPath("FusionCharts/");
 
-	if ( ($idfranquia == '9999999') or (empty($idfranquia)) ){
-		$nome_franquia = 'Todas as Franquias';
-	} else{
-		$selecao = " fra_idfranquia = $idfranquia AND "; 
-		$nome_franquia = nome_franquia($idfranquia);
-	}
-	
-	$data = date('d/m/Y');
-	# Define Charts Parameter
-	$strParam = "caption=Gráfico : VIRTUAL FLEX ;subCaption=$nome_franquia;yAxisName=;xAxisName=;palette=1;animation=".getAnimationState(). ";showValues=1;formatNumberScale=5;numberSuffix=; labelDisplay=ROTATE;numDivLines=5;slantLabels=1";
-	# Set Chart Parameter
-	$FC->setChartParams($strParam);
-	# Get average shiping time xml
-	// grafico_virtualflex($intYear,$selecao,true,false, $FC);
-	$res = grafico_virtualflex_novo($intYear, $selecao, true, false, $FC);
+                if (($idfranquia == '9999999') or (empty($idfranquia))) {
+                    $nome_franquia = 'Todas as Franquias';
+                } else {
+                    $selecao = " a.id_franquia = $idfranquia AND ";
+                    $nome_franquia = nome_franquia($idfranquia);
+                }
+                #Define Charts Parameter
+                $strParam = "caption=Gráfico - WhatsApp Marketing;subCaption=$nome_franquia;yAxisName=;xAxisName=;palette=1;animation=" . getAnimationState() . ";showValues=1;formatNumberScale=5;numberSuffix=; labelDisplay=ROTATE;numDivLines=5;slantLabels=1";
+                # Set Chart Parameter
+                $FC->setChartParams($strParam);
+                # Get average shiping time xml
+                $res = grafico_whatsAppMarketing($intYear, $selecao, true, false, $FC);
                 $i = 0;
 
                 foreach ($res as $key) {
@@ -94,7 +93,7 @@ include("DataGen.php");
                         ]);
 
                         var options = {
-                            title: "Gráfico de Pesquisas",
+                            title: "Gráfico WhatsApp Marketing",
                             width: 1700,
                             height: 800,
                             bar: {
@@ -109,18 +108,24 @@ include("DataGen.php");
                     }
                 </script>
                 <div id="columnchart_values" style="width: 100%; height: 300px;"></div>
-	</td>
-	</tr>
-    
-            <?php if( ($_REQUEST['id_franquia_session'] != '163') and ($_REQUEST['id_franquia_session'] != "") ){ ?>
-    	<tr><td bgcolor="#CCCCCC">&nbsp;</td></tr>
+
+            </td>
+        </tr>
+
+        <?php if (($_REQUEST['id_franquia_session'] != '163') and ($_REQUEST['id_franquia_session'] != "")) { ?>
         <tr>
-        <td align="center">
-          <iframe src="Grafico_Franquia_11.php" width="100%" height="700" frameborder="0" scrolling="0"></iframe>
-        </td>
-        </tr>	
-		    <tr><td height="100">&nbsp;</td></tr>
-    <?php } ?>
-</table>
+            <td bgcolor="#CCCCCC">&nbsp;</td>
+        </tr>
+        <tr>
+            <td align="center">
+                <iframe src="Grafico_Franquia_14.php" width="100%" height="700" frameborder="0" scrolling="0"></iframe>
+            </td>
+        </tr>
+        <tr>
+            <td height="100">&nbsp;</td>
+        </tr>
+        <?php } ?>
+    </table>
 </BODY>
+
 </HTML>
