a9b2b13725a5c871ad0e37f7bff80d52e0de55e3
Note-Luciano\Luciano <lucianomancini@hotmail.com>
2019-12-03 18:38:15 -0200

    mostrar SQL

--- php/graficos/App/DataGen.php
+++ php/graficos/App/DataGen.php
@@ -2951,7 +2951,7 @@ function grafico_franquia_03($intYear, $selecao, $addJSLinks, $forDataURL,&$FC) 
         
         // Function to connect to the DB
         $link = connectToDB();
-        $strSQL = "SELECT 
+        echo "<pre>".$strSQL = "SELECT 
                         COUNT(*) AS Average,
                         CONCAT( 
                             CASE month(a.dh_entrada) 
