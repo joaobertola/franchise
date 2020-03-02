5bb4dd0d0f36815bc3640edff3b98c133fb5bc91
Note-Luciano\Luciano <lucianomancini@hotmail.com>
2020-03-02 13:59:39 -0300

    Gravando sem data e permitindo a exclusao

--- php/area_restrita/a_solicitacao_valores_deposito.php
+++ php/area_restrita/a_solicitacao_valores_deposito.php
@@ -2,7 +2,6 @@
 
 require "../connect/conexao_conecta.php";
 
-
 $id_pedido   = $_REQUEST['id_pedido'];
 $dt_deposito = $_REQUEST['data_deposito'];
 
@@ -11,10 +10,10 @@ if ( trim($dt_deposito) == '' )
 else
     $dt_deposito = substr($dt_deposito,6,4).'-'.substr($dt_deposito,3,2).'-'.substr($dt_deposito,0,2);
 
-$sql_u = "UPDATE cs2.solicitacao_valores
+echo $sql_u = "UPDATE cs2.solicitacao_valores
 			SET
 				data_deposito = '$dt_deposito'
 		  WHERE id = '$id_pedido'";
-$qr2 = mysql_query($sql_u,$con);
+$qr2 = mysql_query($sql_u,$con) or die('Erro Mysql : '.$sql_u);
 
 ?>
