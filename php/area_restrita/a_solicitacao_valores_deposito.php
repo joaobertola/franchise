c33689c2fd12cfa396210729486a1a614ec2c24d
Note-Luciano\Luciano <lucianomancini@hotmail.com>
2020-03-02 13:43:38 -0300

    Gravando sem data e permitindo a exclusao

--- php/area_restrita/a_solicitacao_valores_deposito.php
+++ php/area_restrita/a_solicitacao_valores_deposito.php
@@ -2,13 +2,14 @@
 
 require "../connect/conexao_conecta.php";
 
+$id_pedido   = $_REQUEST['id_pedido'];
 $dt_deposito = $_REQUEST['data_deposito'];
-if ( $dt_deposito == '' )
+
+if ( trim($dt_deposito) == '' )
     $dt_deposito = 'NULL';
 else
     $dt_deposito = substr($dt_deposito,6,4).'-'.substr($dt_deposito,3,2).'-'.substr($dt_deposito,0,2);
 
-$id_pedido   = $_REQUEST['id_pedido'];
 $sql_u = "UPDATE cs2.solicitacao_valores
 			SET
 				data_deposito = '$dt_deposito'
