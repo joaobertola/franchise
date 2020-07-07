02883e2479145430c5a9607837541aeb59b5ebcf
Note-Luciano\Luciano <lucianomancini@hotmail.com>
2020-07-07 17:34:06 -0300

    Ajustes de registros de boletos gerados e enviados para registros

--- php/area_restrita/d_grava_lote_titulos_registrados.php
+++ php/area_restrita/d_grava_lote_titulos_registrados.php
@@ -1,35 +1,50 @@
 <?php
 
-	# Autor:   Luciano Mancini
-	# M�dulo:  Remessa Fornecedor - Banco Bradesco
-	# Finalidade: 
-	#		Gerar o arquivo de cr�dito a serem enviados ao Banco Bradesco 
-	#		para os cliente ANTECIPA��O
+  # Autor:   Luciano Mancini
+  # Modulo:  Remessa Fornecedor - Banco Bradesco
+  # Finalidade: 
+  #   Confirmacao dos registros enviado parra o banco para registro
+  
+  include("../../../validar2.php");
 
-	include("../../../validar2.php");
+  global $conexao,$arquivo;
+  conecex();
 
-	global $conexao,$arquivo;
-	conecex();
+  $boletos = $_REQUEST['numero_boletos'];
+  $data_limite = $_REQUEST['data_limite'];
+  $sequencia_lote = $_REQUEST['sequencia_lote'];
+  $id_controle_banco = $_REQUEST['id_controle_banco'];
+  $tabela = $_REQUEST['tabela'];
 
-        $boletos = $_REQUEST['numero_boletos'];
-        $data_limite = $_REQUEST['data_limite'];
-        $sequencia_lote = $_REQUEST['sequencia_lote'];
-        $id_controle_banco = $_REQUEST['id_controle_banco'];
+  # Gravando o sequencial de registro de pagamento 
+  $sql_registro = "UPDATE cs2.controle_banco 
+                      SET 
+                          sequencia = '$sequencia_lote',
+                          ult_data = '$data_limite'
+                   WHERE id = '$id_controle_banco'";
+  $qry_registro = mysql_query($sql_registro,$conexao);
 
-	# Gravando o sequencial de registro de pagamento 
-	$sql_registro = "UPDATE cs2.controle_banco 
-                            SET 
-                                sequencia = '$sequencia_lote',
-                                ult_data = '$data_limite'
-                         WHERE id = '$id_controle_banco'";
-	$qry_registro = mysql_query($sql_registro,$conexao);
+  if ( $tabela == 'mensalidade' ){
+      
+      $sql_grava_data_envio_lote = 
+             "UPDATE cs2.titulos
+                SET
+                  data_movimentacao = '$data_limite',
+                  data_gravacao_lote = '$data_limite',
+                  num_lote = '$sequencia_lote'
+              WHERE numboleto_bradesco IN ($boletos)";
+      
+  }else{
 
-        $sql_grava_data_envio_lote = 
-               "UPDATE cs2.titulos_recebafacil
-                  SET
-                    data_gravacao_lote = '$data_limite'
-                WHERE numboleto_itau IN ($boletos)";
-        $qry_registro = mysql_query($sql_grava_data_envio_lote,$conexao);
+      $sql_grava_data_envio_lote = 
+             "UPDATE cs2.titulos_recebafacil
+                SET
+                  data_gravacao_lote = '$data_limite'
+              WHERE numboleto_itau IN ($boletos)";
+
+  }
+
+  $qry_registro = mysql_query($sql_grava_data_envio_lote,$conexao);
         
-	echo 'OK';
+  echo 'OK';
 ?>
