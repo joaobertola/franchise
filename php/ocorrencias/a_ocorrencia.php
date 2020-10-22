39f0b3bd7213b461a712eb17d74c291cdac52a92
TI <cpd1@webcontrolempresas.com.br>
2020-10-22 15:39:06 -0300

    status

--- php/ocorrencias/a_ocorrencia.php
+++ php/ocorrencias/a_ocorrencia.php
@@ -77,13 +77,13 @@ window.onload = function() {
 
 if ($go=='ingressar') {
 	if (($tipo == "a") || ($tipo == "c")) {
-		$sql = "select mid(a.logon,1,5) as logon, b.id_franquia, b.codloja from logon a
+		$sql = "select mid(a.logon,1,locate('S',b.logon)-1) as logon, b.id_franquia, b.codloja from logon a
 				inner join cadastro b on a.codloja=b.codloja
-				where mid(logon,1,5)='$codigo'";
+				where mid(logon,1,locate('S',b.logon)-1)='$codigo'";
 	} else {
-		$sql = "select mid(a.logon,1,5) as logon, b.id_franquia, b.codloja from logon a
+		$sql = "select mid(a.logon,1,locate('S',b.logon)-1) as logon, b.id_franquia, b.codloja from logon a
 				inner join cadastro b on a.codloja=b.codloja
-				where mid(logon,1,5)='$codigo' and id_franquia='$id_franquia'";
+				where mid(logon,1,locate('S',b.logon)-1)='$codigo' and id_franquia='$id_franquia'";
 	}
 	$resulta = mysql_query($sql,$con);
 	$linha = mysql_num_rows($resulta);
